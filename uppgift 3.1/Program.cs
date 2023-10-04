07fe753468aa7dc30c6ea91d6d6857ddbd9df6ca
Tareq Bilal Al-Asaad <taal04003@utb.vaxjo.se>
2023-10-04 10:30:40 +0200

    Add project files.

---
+++ uppgift 3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TavlingsAlder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kontrollera din tävlingsålder");
+            Console.Write("Ange din ålder: ");
+
+            int age;
+            if (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("Felaktig ålder. Ange ålder som ett heltal.");
+            }
+            else
+            {
+                switch (age)
+                {
+                    case int n when (n >= 16 && n <= 19):
+                        Console.WriteLine("Du får delta i tävlingen!");
+                        break;
+                    case int n when (n < 16):
+                        Console.WriteLine("Du är för ung för att delta i tävlingen.");
+                        break;
+                    default:
+                        Console.WriteLine("Du är för gammal för att delta i tävlingen.");
+                        break;
+                }
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
+
